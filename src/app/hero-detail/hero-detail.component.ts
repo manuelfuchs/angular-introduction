4fe0dbacaee471c906d8695948afe5b989221f76
Manuel Fuchs <manuel.fuchs@softaware.at>
2025-04-08 10:37:16 +0200

    feat: Add hero detail page

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -2,6 +2,14 @@ import { Component, effect, input } from '@angular/core';
 
 @Component({
     selector: 'app-hero-detail',
-    template: 'hero-detail',
+    template: 'hero-detail {{ heroId() }}',
 })
-export default class HeroDetailComponent {}
+export default class HeroDetailComponent {
+    public readonly heroId = input<number>();
+
+    constructor() {
+        effect(() => {
+            console.log('Hero ID:', this.heroId());
+        });
+    }
+}
