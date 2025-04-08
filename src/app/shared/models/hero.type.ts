c50b4becf7c52235abcd3248dff1a7a5f44d3ee2
Manuel Fuchs <manuel.fuchs@softaware.at>
2025-04-08 10:01:16 +0200

    feat: Add hero interface

---
+++ src/app/shared/models/hero.type.ts
@@ -0,0 +1,10 @@
+export interface Hero {
+    id: number;
+    name: string;
+}
+
+export const defaultHeroes: Hero[] = [
+    { id: 12, name: 'Dr. Nice' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+];
