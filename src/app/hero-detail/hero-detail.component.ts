4a6ae0deaf1e4daaf89742d5af8967b2de28211e
Manuel Fuchs <manuel.fuchs@softaware.at>
2025-04-08 10:51:28 +0200

    feat: Create hero-detail form

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,15 +1,44 @@
-import { Component, effect, input } from '@angular/core';
+import { Component, effect, inject, input } from '@angular/core';
+import { createHeroDetailForm } from './hero-detail-form.type';
+import { NonNullableFormBuilder } from '@angular/forms';
+import { AppState } from '../shared/app.state';
 
 @Component({
     selector: 'app-hero-detail',
-    template: 'hero-detail {{ heroId() }}',
+    template: `
+        <div class="flex flex-col">
+            <span>hero-detail</span>
+            <span>{{ heroId() }}</span>
+            <span>{{ form.controls.name.value }}</span>
+        </div>
+    `,
 })
 export default class HeroDetailComponent {
-    public readonly heroId = input<number>();
+    private readonly fb = inject(NonNullableFormBuilder);
+    private readonly appState = inject(AppState);
+
+    public readonly heroId = input<string>();
+
+    protected readonly form = createHeroDetailForm(this.fb);
 
     constructor() {
+        // Initialize form with existing hero data
         effect(() => {
-            console.log('Hero ID:', this.heroId());
+            const heroId = this.heroId();
+            if (heroId === undefined) {
+                return;
+            }
+
+            const hero = this.appState
+                .heroes()
+                .find((h) => `${h.id}` === heroId);
+            if (hero === undefined) {
+                return;
+            }
+
+            this.form.patchValue({
+                name: hero.name,
+            });
         });
     }
 }
