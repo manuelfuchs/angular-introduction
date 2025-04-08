f6a6da35b1dcffe0c13ff6ce021434da8f581152
Manuel Fuchs <manuel.fuchs@softaware.at>
2025-04-08 10:01:28 +0200

    feat: Add heroes list template

--- src/app/hero-list/hero-list.component.ts
+++ src/app/hero-list/hero-list.component.ts
@@ -1,7 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { defaultHeroes } from '../shared/models';
 
 @Component({
     selector: 'app-hero-list',
-    template: 'hero list',
+    template: `
+        <h1 class="text-2xl mb-4">Heroes</h1>
+
+        <ul class="flex flex-col gap-3">
+            @for(hero of heroes(); track hero.id) {
+            <div
+                class="h-16 flex items-center border-neutral-200 border rounded-md pl-4"
+            >
+                <span class="grow">{{ hero.name }}</span>
+                <button
+                    class="bg-neutral-100 hover:bg-neutral-200 h-full px-16"
+                >
+                    Edit
+                </button>
+            </div>
+            }
+        </ul>
+    `,
 })
-export default class HeroListComponent {}
+export default class HeroListComponent {
+    protected readonly heroes = signal(defaultHeroes);
+}
