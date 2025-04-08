bd98a4109afa7064667f58b518f5170dcb3ada9b
Manuel Fuchs <manuel.fuchs@softaware.at>
2025-04-08 08:29:09 +0200

    chore: Fix eslint error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { RouterOutlet } from '@angular/router';
     templateUrl: './app.component.html',
 })
 export class AppComponent {
-    title = 'angular-introduction';
+    protected title = 'angular-introduction';
 }
