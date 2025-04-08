74e7e4f57b4f6c448dbc52ca3d19ac0f9e9ddec9
Manuel Fuchs <manuel.fuchs@softaware.at>
2025-04-08 09:18:17 +0200

    chore: Remove demo code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
+    template: 'app-root',
 })
-export class AppComponent {
-    protected title = 'angular-introduction';
-}
+export class AppComponent {}
